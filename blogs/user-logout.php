c3ad2264c216cb6de7f9082e5769897d8c7eb9d8
Eshan30 <eshanjain300401@gmail.com>
2022-06-17 20:38:18 +0530

    final

--- blogs/user-logout.php
+++ blogs/user-logout.php
@@ -3,8 +3,9 @@
     $usignnm = $_SESSION['usignnm'];
     echo"<script language='javascript'>
                  alert('$usignnm you are logged out successfully...Please Visit Again');
+                 window.location.href = '../user_signup/user-signup.html';
             </script>";
     session_unset();
     session_destroy();
-    header('location: ../user_signup/user-signup.html');
+    // header('location: ../user_signup/user-signup.html');
 ?>
